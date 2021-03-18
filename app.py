206103b0d3fa09b51c5f8ffdc7c2388bbce42bd9
Robzlegz <robzlegz@gmail.com>
2021-03-18 15:57:59 +0200

    checking keydowns

--- app.py
+++ app.py
@@ -4,13 +4,25 @@ pygame.init()
 
 COLOR = [1, 1, 1]
 
+ufo_image = pygame.image.load("ufo.png")
+
 while True:
-    window = pygame.display.set_mode([715, 480])
+    window = pygame.display.set_mode([800, 500])
     pygame.display.set_caption("Starship")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print("UP")
+            if event.key == pygame.K_a:
+                print("LIFT")
+            if event.key == pygame.K_d:
+                print("RIGHT")
+            if event.key == pygame.K_s:
+                print("DOWN")
 
     window.fill(COLOR)
+    window.blit(ufo_image, [380, 210])
     pygame.display.update()
